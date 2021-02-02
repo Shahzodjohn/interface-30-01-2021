a52c592a7d7dbc1a043e91459cfb63c85e907a3a
User <User@WIN-5RSC18PG333>
2021-02-02 16:47:35 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    interface Iplayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : Iplayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+        public void Record()
+        {
+            Console.WriteLine("Record");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Iplayable iplay = new Player();
+            IRecodable irec = new Player();
+            iplay.Play();
+            iplay.Pause();
+            iplay.Stop();
+            irec.Pause();
+            irec.Record();
+            irec.Stop();
+
+
+
+
+        }
+    }
+}
